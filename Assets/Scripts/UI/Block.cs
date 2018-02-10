dffa4b9bea58ce1230dc1a994df1302f4b639da3
Lefan Tan Jia Hui <lefantan@hotmail.com>
2018-02-10 05:28:21 +0000

    No Comments

---
+++ Assets/Scripts/UI/Block.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	public event System.Action<Block> OnBlockClick;
+	public Vector2 offset;
+	public float offsetTime;
+
+	public void OnClick(){
+
+		if (OnBlockClick != null) {
+			OnBlockClick (this);
+		}
+	}
+
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	List<Block> blocksToAnimate = new List<Block>();
+
+	void Start(){
+		blocksToAnimate.AddRange(FindObjectsOfType<Block>());
+	}
+
+	void MoveBlock(Block block){
+		StartCoroutine (MoveAnimation(block.transform, block.offset, block.offsetTime));
+	}
+
+	IEnumerator MoveAnimation(Transform transform, Vector2 offset, float offsetTime){
+		float percent = 0;
+		Vector2 startingPos = transform.position;
+
+		while(percent > 1.0f){
+			percent += Time.deltaTime / offsetTime;
+			transform.position = Vector2.Lerp (startingPos, startingPos + offset, percent);
+			yield return null;
+		}
+
+
+	}
+
+
+}
