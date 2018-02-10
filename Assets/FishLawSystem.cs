a87939c8ea86a64e7af2c51186ed6f8893b65ae2
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2018-02-10 02:43:13 +0000

    added fish law system script

---
+++ Assets/FishLawSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishLawSystem : MonoBehaviour {
+
+	public Vector2 currentCoordinates;
+	[SerializeField]
+	private int updateRate = 5;
+
+	private void Start () {
+		
+	}
+
+	private IEnumerator SourceGPSLocation () {
+		while (true) {
+			yield return new WaitForSeconds (updateRate);
+			// source GPS for current coords, set curretn coords to current location
+		}
+	}
+
+}
