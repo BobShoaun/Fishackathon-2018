90f521b347e865645d251d997403179303aa7b1a
Lefan Tan Jia Hui <lefantan@hotmail.com>
2018-02-10 16:23:42 +0000

    No Comments

---
+++ Assets/Scripts/UI/Step.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class Step {
+
+	public TextMeshProUGUI text;
+	public int order;
+
+}
---
+++ Assets/Scripts/UI/TutorialManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+	int firstTime;
+
+	// Use this for initialization
+	void Start () {
+		firstTime = PlayerPrefs.GetInt ("isFirstTime");
+
+		if (firstTime == 0)
+			StartTutorial ();
+		else
+			PlayerPrefs.SetInt ("isFirstTime", 1);
+
+	}
+
+	void StartTutorial(){
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
