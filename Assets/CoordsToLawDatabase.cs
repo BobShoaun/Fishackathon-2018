94876060f4de55dfe9615ff067e3e52aae1e9251
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2018-02-10 03:17:02 +0000

    fish database tested

--- Assets/CoordsToLawDatabase.cs
+++ Assets/CoordsToLawDatabase.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CoordsToLawDatabase : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Law[] Laws;
+
+	public IEnumerable<Law> this [Vector2 coords] {
+		get { 
+			return Laws.Where (law => law.coordinates.Contains (coords));
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Start () {
+		Laws = new Law[] {
+			new Law (new Vector2 [] { 
+				new Vector2 (121, 300), 
+				new Vector2 (300, 400), 
+				new Vector2 (400, 323)}, "You cannot fish with a net here!"),
+			new Law (new Vector2 [] { 
+				new Vector2 (121, 300), 
+				new Vector2 (222, 333), 
+				new Vector2 (111, 222)}, "DO NOT FISH HERE ITS ILLEGAL"),
+			new Law (new Vector2 [] { 
+				new Vector2 (555, 444), 
+				new Vector2 (398, 120), 
+				new Vector2 (900, 1090)}, "YOU CAN FISH ANYTHING U WANT")
+		};
 	}
+
+}
+
+public class Law {
+
+	public Vector2 [] coordinates;
+	public string statement;
+
+	public Law (Vector2[] coords, string statement) {
+		coordinates = coords;
+		this.statement = statement;
+	}
+
+	public override string ToString ()
+	{
+		return statement;
+	}
+
 }
--- Assets/FishLawSystem.cs
+++ Assets/FishLawSystem.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FishLawSystem : MonoBehaviour {
+
+	public CoordsToLawDatabase database;
 
 	public Vector2 currentCoordinates;
 	[SerializeField]
 	private int updateRate = 5;
 
+	public string Longitude { get; set; }
+	public string Lattitude { get; set; }
+
+	public TextMeshProUGUI lawText;
+
 	private void Start () {
-		
+		database = GetComponent<CoordsToLawDatabase> ();
 	}
+
+	public void SourceDatabaseFromCoords () {
+		float longitude = float.Parse (Longitude);
+		float lattitude = float.Parse (Lattitude);
+		Vector2 coords = new Vector2 (longitude, lattitude);
+
+		string lawString = "The Law in this coords are ";
+
+
+		foreach (Law law in database [coords])
+			lawString += law;
+
+		lawText.text = lawString;
+	}
+
 
 	private IEnumerator SourceGPSLocation () {
 		while (true) {
